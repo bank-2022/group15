c31905a8700ec075352afc3fab331cce11b2569e
Shambambulish <terho.traff@gmail.com>
2022-05-01 05:51:44 +0300

    luetunymmärtämistä

--- routes/login.js
+++ routes/login.js
@@ -16,7 +16,7 @@ router.post('/',
           }
           else{
             if (dbResult.length > 0) {
-              if(dbResult.lockedTime == "") {
+              if(dbResult[0].lockedTime) {
                 if(bcrypt.compareSync(password, dbResult[0].pin)) {
                   console.log("success");
                   const token = generateAccessToken({username: username})
